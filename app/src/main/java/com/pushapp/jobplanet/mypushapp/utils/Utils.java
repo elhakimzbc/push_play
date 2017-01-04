70cdee106a26d211b108131b903cfb3cc65bfb11
Abil Hakim <el.hakimz@braincommerce.com>
2017-01-04 11:19:30 +0700

    first commit

---
+++ app/src/main/java/com/pushapp/jobplanet/mypushapp/utils/Utils.java
@@ -0,0 +1,76 @@
+package com.pushapp.jobplanet.mypushapp.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by jobplanet on 1/4/17.
+ */
+
+public class Utils {
+
+    /**
+     * check network availability
+     * @param ctx
+     * @return
+     */
+    public static boolean isNetworkAvailable( Context ctx ){
+        boolean connected = false;
+
+        ConnectivityManager connMan = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] info = connMan.getAllNetworkInfo();
+
+        for (NetworkInfo anInfo : info) {
+            if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
+                Toast.makeText(ctx, "Internet is connected", Toast.LENGTH_SHORT).show();
+                connected = true;
+            }
+        }
+        return connected;
+    }
+
+
+    /**
+     * check server service availability
+     * @param urlStr
+     * @return
+     */
+    public static InputStream openHttpConnection(String urlStr) {
+        InputStream in = null;
+        int resCode = -1;
+
+        try {
+            URL url = new URL(urlStr);
+            URLConnection urlConn = url.openConnection();
+
+            if (!(urlConn instanceof HttpURLConnection)) {
+                throw new IOException("URL is not an Http URL");
+            }
+
+            HttpURLConnection httpConn = (HttpURLConnection) urlConn;
+            httpConn.setAllowUserInteraction(false);
+            httpConn.setInstanceFollowRedirects(true);
+            httpConn.setRequestMethod("GET");
+            httpConn.connect();
+            resCode = httpConn.getResponseCode();
+
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                in = httpConn.getInputStream();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return in;
+    }
+
+}
---
+++ app/src/main/java/com/pushapp/jobplanet/mypushapp/utils/VolleyRequestor.java
@@ -0,0 +1,45 @@
+package com.pushapp.jobplanet.mypushapp.utils;
+
+/**
+ * Created by jobplanet on 1/4/17.
+ */
+
+public class VolleyRequestor {
+
+
+    public void requestString(String url){
+
+    }
+
+    public void requestJson(String url){
+
+    }
+
+    public void requestJsonArray(String url){
+
+    }
+
+    public void imageLoader(String url){
+
+    }
+
+    public void requestCache(String url){
+
+    }
+
+    public void invalidateCache(String url){
+
+    }
+
+    public void deleteCache(String url){
+
+    }
+
+    public void clearCache(String url){
+
+    }
+
+
+
+
+}
